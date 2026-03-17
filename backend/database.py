94f7e74774e71e92cd268c8e0a88477dc47e9231
root <root@ip-10-0-10-223.ap-south-1.compute.internal>
2026-03-17 10:39:09 +0000

    sever updated

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-# Format: postgresql://username:password@localhost:5432/database_name
-SQLALCHEMY_DATABASE_URL = "postgresql://admin_user:CiCD2026@localhost:5432/deployer_db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-    # No check_same_thread=False here!
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://admin_user:CiCD2026@db:5432/deployer_db"
 )
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
